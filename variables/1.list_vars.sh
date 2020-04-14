0dee7fbc5b82e4777d7ac5f0d8e9d51872fb6308
masa3 <masa@hashicorp.com>
2020-04-14 13:51:03 +0900

    updated vars

--- variables/1.list_vars.sh
+++ variables/1.list_vars.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#if [ $# -eq 0 ]
-#	then
-#		echo 'Needs to supply argument'
-#		echo '  $1 = <arg>'
-#		exit 1
-#fi
+if [ $# -ne 2 ]
+	then
+		echo 'Needs to supply argument'
+		echo '  $1 = org'
+		echo '  $2 = workspace'
+		exit 1
+fi
 
-curl   --header "Authorization: Bearer ${TOKEN}"   --header "Content-Type: application/vnd.api+json"   https://${TFE_SERVER}/api/v2/workspaces/ws-k79BrnepJ1rqyNNa/vars
+WORKSPACE_ID=$(curl --header "Authorization: Bearer ${TOKEN}"   --header "Content-Type: application/vnd.api+json"   https://${TFE_SERVER}/api/v2/organizations/${1}/workspaces/${2} | jq -r '.data.id')
+
+curl --header "Authorization: Bearer ${TOKEN}" --header "Content-Type: application/vnd.api+json" https://${TFE_SERVER}/api/v2/workspaces/${WORKSPACE_ID}/vars
---
+++ variables/4.delete_vars.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]
+	then
+	echo 'Needs to supply argument'
+	echo '  $1 = org'
+	echo '  $2 = workspace'
+	echo '  $3 = var'
+		exit 1
+fi
+
+WORKSPACE_ID=$(curl --header "Authorization: Bearer ${TOKEN}"   --header "Content-Type: application/vnd.api+json"   https://${TFE_SERVER}/api/v2/organizations/${1}/workspaces/${2} | jq -r '.data.id')
+
+FILTER=".data[] | select(.attributes.key == \"${3}\") | .id"
+VAR_ID=$(curl --header "Authorization: Bearer ${TOKEN}" --header "Content-Type: application/vnd.api+json" https://${TFE_SERVER}/api/v2/workspaces/${WORKSPACE_ID}/vars | jq -r "$FILTER" )
+
+curl --header "Authorization: Bearer ${TOKEN}" --header "Content-Type: application/vnd.api+json" --request DELETE https://${TFE_SERVER}/api/v2/workspaces/${WORKSPACE_ID}/vars/${VAR_ID} 
